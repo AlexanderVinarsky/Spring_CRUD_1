45e4104e71c47620b83ebe37716ed5c36881c109
AlexanderVinarsky <vinarskydichtv@gmail.com>
2022-12-14 00:03:31 +0300

    Initial Commit

---
+++ src/main/java/cordell/DAO/PersonDAO.java
@@ -0,0 +1,34 @@
+package cordell.DAO;
+
+import cordell.models.Person;
+import org.springframework.stereotype.Component;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private static List<Person> people;
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike"));
+    }
+
+    public static List<Person> index(){
+        return people;
+    }
+
+    public Person show(int id){
+        return people.stream().filter(person->person.getId()==id).findAny().orElse(null);
+    }
+
+    public void save(Person person){
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
+}
