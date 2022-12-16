6a7cc56b14d18decb99b4d340739ec884a8bfc43
AlexanderVinarsky <vinarskydichtv@gmail.com>
2022-12-16 21:22:58 +0300

    JDBC Template Update

--- src/main/java/cordell/DAO/PersonDAO.java
+++ src/main/java/cordell/DAO/PersonDAO.java
@@ -1,6 +1,8 @@
 package cordell.DAO;
 
 import cordell.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import java.sql.*;
@@ -9,91 +11,31 @@ import java.util.List;
 
 @Component
 public class PersonDAO {
-    private static final String URL = "jdbc:postgresql://localhost:5432/CRUD_Test";
-    private static final String USERNAME = "postgres";
-    private static final String PASSWORD = "89225523232Alex";
 
-    private static Connection connection;
-    static {
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDAO(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
     }
 
-    public static List<Person> showPeople(){
-        List<Person> people = new ArrayList<>();
-        try {
-            Statement statement = connection.createStatement();
-            String SQL = "SELECT * FROM people";
-            ResultSet resultSet = statement.executeQuery(SQL);
-
-            while(resultSet.next()) {
-                Person person = new Person();
-                person.setId(resultSet.getInt("id"));
-                person.setName(resultSet.getString("name"));
-
-                people.add(person);
-            }
-
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-        return people;
+    public List<Person> showPeople() {
+        return jdbcTemplate.query("SELECT * FROM People", new PersonMapper());
     }
-
     public Person showPerson(int id){
-        try {
-            String sql = "SELECT * FROM people WHERE id = ?";
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setInt(1, id);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            resultSet.next();
-            Person person = new Person();
-            person.setId(resultSet.getInt("id"));
-            person.setName(resultSet.getString("name"));
-            return person;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        return jdbcTemplate.query("SELECT * FROM People WHERE id = ?", new Object[]{id}, new PersonMapper())
+                .stream().findAny().orElse(null);
     }
 
     public void save(Person person) {
-        try{
-            String sql = "INSERT INTO people VALUES(DEFAULT, ?)";
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setString(1, person.getName());
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        jdbcTemplate.update("INSERT INTO people VALUES(DEFAULT, ?)", person.getName());
     }
 
     public void update(int id, Person updatedPerson) {
-        try {
-            String sql = "UPDATE people SET name= ? where id =?";
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setString(1, updatedPerson.getName());
-            preparedStatement.setInt(2, id);
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        jdbcTemplate.update("UPDATE people SET name= ? where id =?", updatedPerson.getName(), updatedPerson.getId());
     }
 
     public void delete(int id){
-        try{
-            Statement statement = connection.createStatement();
-            String sql = "DELETE FROM People WHERE id =" + id;
-            statement.executeUpdate(sql);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        jdbcTemplate.update("DELETE FROM people where id = ?", id);
     }
 }
